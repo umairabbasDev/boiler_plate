560e0ee439b013c99ab3ddc8565d055ff40eac21
xxx <blackfire@localhost.localdomain>
2020-12-29 16:42:35 +0500

    new commit

---
+++ src/components/Hero.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div>
+      <div className="hero-image">
+        <div>
+          <h1 className="hero-text">Generation</h1>
+          <button className="hero-btn">Click Here</button>
+        </div>
+      </div>
+
+      <div class="fea-section">
+        <h3 className="fea-heading">Services</h3>
+
+        <ul className="fea-imgs">
+          <li className="fea-item">
+            <img   width="350px" height="200px" src="https://images.unsplash.com/photo-1524514587686-e2909d726e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" />
+            <div className="block"></div>
+            <h2 className="fea-text"> Generator Repair </h2>
+          </li>
+          <li className="fea-item">
+            <img   width="350px" height="200px" src="https://images.unsplash.com/photo-1524514587686-e2909d726e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" />
+            <div className="block"></div>
+            <h2 className="fea-text"> Generator Repair </h2>
+          </li>
+          <li className="fea-item">
+            <img   width="350px" height="200px" src="https://images.unsplash.com/photo-1524514587686-e2909d726e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" />
+            <div className="block"></div>
+            <h2 className="fea-text"> Generator Repair </h2>
+          </li>
+          <li className="fea-item">
+            <img   width="350px" height="200px" src="https://images.unsplash.com/photo-1524514587686-e2909d726e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" />
+            <h2 className="fea-text"> Generator Repair </h2>
+            <div className="block"></div>
+          </li>
+          <li className="fea-item">
+            <img   width="350px" height="200px" src="https://images.unsplash.com/photo-1524514587686-e2909d726e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" />
+            <div className="block"></div>
+            <h2 className="fea-text"> Generator Repair </h2>
+          </li>
+          <li className="fea-item">
+            <img   width="350px" height="200px" src="https://images.unsplash.com/photo-1524514587686-e2909d726e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" />
+            <div className="block"></div>
+            <h2 className="fea-text"> Generator Repair </h2>
+          </li>
+          <li className="fea-item">
+            <img   width="350px" height="200px" src="https://images.unsplash.com/photo-1524514587686-e2909d726e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" />
+            <div className="block"></div>
+            <h2 className="fea-text"> Generator Repair </h2>
+          </li>
+          <li className="fea-item">
+            <img   width="350px" height="200px" src="https://images.unsplash.com/photo-1524514587686-e2909d726e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" />
+            <div className="block"></div>
+            <h2 className="fea-text"> Generator Repair </h2>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
